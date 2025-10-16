ca35e70a54ed4e40b57ff45230f4695ad5f8e8da
Jaydar Mitchell <jaydarmitchell@gmail.com>
2025-10-15 20:22:47 -0500

    newcommit

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,19 +1,46 @@
 //alert("Code not working");
-
-    window.onload = function() {
-        const boardgame = document.getElementById("board");
-        const boardelements = boardgame.getElementsByTagName("div");
-
-        // Add a "board" class (optional)
-        boardgame.classList.add("board");
+window.onload = function() {
+    const boardgame = document.getElementById("board");
+    const boardelements = boardgame.getElementsByTagName("div");
 
 
-        for (let i = 0; i < boardelements.length; i++) {
-            boardelements[i].classList.add("square");
-        }
 
 
-        //alert("JavaScript is running!");
-    };
+
+    boardgame.classList.add("board");
 
 
+    let X = true;
+
+
+    let boardState = Array(9).fill("");
+
+    for (let i = 0; i < boardelements.length; i++) {
+        const square = boardelements[i];
+        square.classList.add("square");
+
+
+        square.addEventListener("click", function() {
+
+
+            if (square.textContent !== "") return;
+
+            if (X) {
+                square.textContent = "X";
+                square.classList.add("X");
+                boardState[i] = "X";
+            } else {
+                square.textContent = "O";
+                square.classList.add("O");
+                boardState[i] = "O";
+            }
+
+
+
+            X = !X;
+
+
+            //console.log(boardState);
+        });
+    }
+};
